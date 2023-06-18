d1696b733f137941ce04ad3f634460799b4a50ad
Hayeon Jeong <hayeon.jeong@berkeley.edu>
2023-06-19 02:32:23 +0900

    Debugging

---
+++ lab/lab02/src/bomb/BombMain.java
@@ -0,0 +1,23 @@
+package bomb;
+
+import common.IntList;
+
+public class BombMain {
+    public static <address> void main(String[] args) {
+        int phase = 2;
+        if (args.length > 0) {
+            phase = Integer.parseInt(args[0]);
+        }
+        // TODO: Find the correct passwords to each phase using debugging techniques
+        Bomb b = new Bomb();
+        if (phase >= 0) {
+            b.phase0("39291226");
+        }
+        if (phase >= 1) {
+            b.phase1(IntList.of(0, 9, 3, 0, 8)); // Figure this out too
+        }
+        if (phase >= 2) {
+            b.phase2("-81201430 ".repeat(1338));
+        }
+    }
+}
